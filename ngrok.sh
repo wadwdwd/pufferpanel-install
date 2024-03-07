906aad77db6cc6a81103141c4350956161935d54
wadwdwd <127041383+wadwdwd@users.noreply.github.com>
2024-03-06 23:08:20 -0600

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 # FOR x86 servers using linux : 
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
-./ngrok config add-authtoken 2XWvdIQegnz7fRUXkMvJWVy7Z5p_3SijirFvadrRgBJot32fG #Put Yours here
+./ngrok config add-authtoken 2NwSxaVbW4yd7isoXZO6BN19Ffd_7Y8vrBYsqsZ1cUDiU781v
 ./ngrok http 8080
